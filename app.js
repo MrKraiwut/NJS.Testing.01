5d6e416f2ade0d1100592aaf89f225057772ef53
MrKraiwut <92616498+MrKraiwut@users.noreply.github.com>
2023-10-17 16:15:39 +0700

    Update Template
    Create Template

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express   = require('express');
-const chalk = require('chalk')
-const debug = require('debug')
-const morgan = require('morgan')
+const chalk = require('chalk');
+const debug = require('debug');
+const morgan = require('morgan');
+const path  = require('path');
+
 const app = express();
 const port = 3000;
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname,"/public/")))
 
 app.get("/", (req,res) =>{
 
@@ -14,6 +17,5 @@ app.get("/", (req,res) =>{
 })
 
 app.listen(port, () => {
-
     debug("Listining on port" + chalk.green(port));
 })
